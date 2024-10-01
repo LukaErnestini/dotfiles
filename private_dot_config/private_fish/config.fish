eaf6ba3870583b6aa8a1f913dd375cd874573602
Luka Ernestini <luka.ernestini@gmail.com>
2024-10-01 21:33:25 +0200

    remove some more stuff

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,17 +1,3 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-
-source ~/cursor.sh
-
-# Set DOTNET_ROOT
-set -gx DOTNET_ROOT $HOME/.dotnet
-
-# Add .NET to PATH
-fish_add_path $DOTNET_ROOT $DOTNET_ROOT/tools
-# pnpm
-set -gx PNPM_HOME "/home/lukaernestini/.local/share/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end
